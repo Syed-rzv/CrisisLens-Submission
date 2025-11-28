09071794b3399772924b07d675b85b348e596cd2
Abbas <abbs924@gmail.com>
2025-11-28 18:38:23 +0400

    Reducing clutter in forecast batch script

--- run_arima_forecast.bat
+++ run_arima_forecast.bat
@@ -1,8 +1,6 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM ARIMA Forecast Service - Daily Automated Run
-
 set LOG_DIR=%~dp0logs
 if not exist "%LOG_DIR%" mkdir "%LOG_DIR%"
 
@@ -16,20 +14,18 @@ for /f "tokens=1-6 delims=/:. " %%a in ("%date% %time%") do (
 )
 set LOGFILE=%LOG_DIR%\arima_forecast_%YYYY%%MM%%DD%_%HH%%Min%%Sec%.txt
 
-echo ============================= >> "%LOGFILE%"
 echo ARIMA Forecast Run >> "%LOGFILE%"
 echo Started at %date% %time% >> "%LOGFILE%"
-echo ============================= >> "%LOGFILE%"
+echo. >> "%LOGFILE%"
 
 cd /d "%~dp0crisislens-API"
 C:\Users\97150\anaconda3\python.exe arima_forecast_service.py --periods 30 --by-type >> "%LOGFILE%" 2>&1
 set EXITCODE=%ERRORLEVEL%
 
 if %EXITCODE%==0 (
-    echo SUCCESS: Forecast completed at %date% %time% >> "%LOGFILE%"
+    echo Forecast completed at %date% %time% >> "%LOGFILE%"
 ) else (
-    echo ERROR: Forecast failed with exit code %EXITCODE% >> "%LOGFILE%"
+    echo Failed with exit code %EXITCODE% >> "%LOGFILE%"
 )
 
-echo. >> "%LOGFILE%"
 endlocal
